9396cb0d48f798260ca8f0d238d1359397598385
chalo <lpalaciosayala@gmail.com>
2025-08-06 11:46:46 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repo-releases",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Chalo Devlgpa",                         # Tu nombre
+    author_email="lpalaciosayala@gmail.comm",                 # Tu correo electrónico
+    url="https://github.com/devlgpa/Repo-Releases",     # URL del proyecto
+)
